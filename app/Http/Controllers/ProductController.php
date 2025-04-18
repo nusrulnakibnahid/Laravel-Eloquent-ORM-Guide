c9b2d37a026c8fe94f2cd512ad24d2b2fee04c3e
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-04-18 17:54:34 +0600

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function getProductByQuery(){
+        $products = DB::table('products')->get();
+        return $products;
+    }
+}
